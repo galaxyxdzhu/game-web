5c2ed3e3b42b92b720de7a8ccfc2610256900d7f
zhuxiaodong <galaxyzhu@gmail.com>
2022-08-04 22:57:32 +0800

    feat: 首次提交

--- src/main.js
+++ src/main.js
@@ -2,11 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './style/index.scss'
+
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
+
+import './utils/flexable'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
